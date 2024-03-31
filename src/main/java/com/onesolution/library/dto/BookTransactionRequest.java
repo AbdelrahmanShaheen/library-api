21cfb511dbff622ff8e08b535dac1f6686587945
AbdelrahmanShaheen <shaheenabdelrahman28@gmail.com>
2024-03-31 17:11:36 +0200

    add validations to BookTransactionRequst

--- src/main/java/com/onesolution/library/dto/BookTransactionRequest.java
+++ src/main/java/com/onesolution/library/dto/BookTransactionRequest.java
@@ -1,5 +1,7 @@
 package com.onesolution.library.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,5 +12,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @Data
 public class BookTransactionRequest {
+    @NotEmpty(message = "Borrower email is required")
+    @Email(message = "Invalid email format")
     private String borrowerEmail;
 }
