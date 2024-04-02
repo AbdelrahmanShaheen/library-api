ecf7302fd0d78ca8affb9433b135225c2a9b4534
AbdelrahmanShaheen <shaheenabdelrahman28@gmail.com>
2024-04-02 23:27:42 +0200

    minor change2

--- src/main/java/com/onesolution/library/service/BookTransactionService.java
+++ src/main/java/com/onesolution/library/service/BookTransactionService.java
@@ -2,21 +2,9 @@ package com.onesolution.library.service;
 
 import com.onesolution.library.dto.BookTransactionRequest;
 import com.onesolution.library.dto.BorrowedBookResponse;
-import com.onesolution.library.entity.Book;
-import com.onesolution.library.entity.BookTransaction;
-import com.onesolution.library.entity.Status;
-import com.onesolution.library.exception.ConflictException;
-import com.onesolution.library.exception.ResourceNotFoundException;
-import com.onesolution.library.repository.BookRepository;
-import com.onesolution.library.repository.BookTransactionRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public interface BookTransactionService {
